a78143c30e8a8487b0eabdb6c40a3f2b169e2ddb
p54507jm <joshua.mather@student.manchester.ac.uk>
2020-03-13 16:21:52 +0000

    on excercise 3

--- lab7/Workshop7.java
+++ lab7/Workshop7.java
@@ -6,33 +6,63 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
+
+// javac11 --module-path ./lib/ --add-modules=javafx.controls Workshop7.java
+// java11 --module-path ./lib/ --add-modules=javafx.controls Workshop7
 
 public class Workshop7 extends Application{
       @Override
       public void start (Stage stage){
-      // Do
-      // something
-      // here
+
       Circle sun = new Circle(50, 50, 40);
       sun.setFill(Color.YELLOW);
 
       VBox root = new VBox(10);
       root.setAlignment(Pos.TOP_RIGHT);
+      String style = "-fx-background-color: rgba(0, 0, 225, 1.0);";
+      root.setStyle(style);
+
 
       VBox vbox1 = new VBox();
       vbox1.setAlignment(Pos.BOTTOM_CENTER);
 
       Rectangle house = new Rectangle(100,100);
-      house.setFill(Color.GRAY);
+      house.setFill(Color.WHITE);
+
+      house.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent t) {
+                if(sun.getFill().equals(Color.YELLOW)){
+                  String style2 = "-fx-background-color: rgba(0, 0, 0, 1.0);";
+                  root.setStyle(style2);
+                  sun.setFill(Color.WHITE);
+                }else{
+                  String style2 = "-fx-background-color: rgba(0, 0, 225, 1.0);";
+                  root.setStyle(style2);
+                  sun.setFill(Color.YELLOW);
+                }
+            }
+        });
+
 
       Rectangle grass = new Rectangle(300,150);
       grass.setFill(Color.GREEN);
 
+
       Polygon roof = new Polygon();
       roof.getPoints().addAll(new Double[]{
-            0.0, 0.0,
+            100.0, 100.0,
             200.0, 100.0,
             100.0, 200.0 });
+
+      roof.setFill(Color.RED);
 
       vbox1.getChildren().addAll(roof, house, grass);
 
@@ -41,6 +71,26 @@ public class Workshop7 extends Application{
       root.getChildren().addAll(sun, vbox1);
 
       Scene scene = new Scene(root);
+
+      /*scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case PLUS:  sun.setRadius(sun.getRadius()+1);
+                    //case DOWN:  goSouth = true; break;;
+                }
+            }
+        });*/
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            if (e.getCode() == KeyCode.ADD) {
+                sun.setRadius(sun.getRadius()+1);
+            }else if (e.getCode() == KeyCode.SUBTRACT) {
+                sun.setRadius(sun.getRadius()-1);
+            }
+
+        });
+
       stage.setScene(scene);
       stage.setTitle("Workshop7");
       stage.show();
---
+++ lab7/Workshop73.java
@@ -0,0 +1,49 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class Workshop73 extends Application{
+
+      @Override
+      public void start (Stage stage){
+      //Define context
+      HBox pane = new HBox(10);
+      pane.setAlignment(Pos.CENTER);
+
+      //Create button and event handler. Defined in the lecture as Mode 2
+      Button bOK = new Button("OK");
+      bOK.setOnAction(new EventHandler<ActionEvent>(){
+        @Override
+        public void handle(final ActionEvent AE){
+          System.out.println("OK button clicked");
+        }
+      });
+
+      //Create button and event handler. Defined in the lecture as Mode 1
+      Button bCancel = new Button("Cancel");
+      CancelHandlerClass handler2 = new CancelHandlerClass();
+      bCancel.setOnAction(handler2);
+
+      pane.getChildren().addAll(bOK, bCancel);
+      Scene scene = new Scene(pane,200,200);
+
+      stage.setScene(scene);
+      stage.setTitle("Workshop7c");
+      stage.show();
+    }
+
+    public static void main(String args[]){        
+      launch(args);
+    }
+
+    class CancelHandlerClass implements EventHandler<ActionEvent>{
+      @Override
+      public void handle(final ActionEvent AE){
+        System.out.println("Cancel button clicked");
+      }
+    }}
